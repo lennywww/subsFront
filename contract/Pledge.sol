80fadf37d9bc3a09db2e1eebcbe947f6b633b20a
fuyundian <985928112@qq.com>
2021-08-21 17:34:28 +0800

    更新质押

--- contract/Pledge.sol
+++ contract/Pledge.sol
@@ -243,7 +243,7 @@ contract Pledge{
 
     Counters.Counter private _tokenIdTracker;
 
-    uint256  public three_months = 90 days;
+    uint256  public three_months = 1 minutes;
 
 
     constructor(address quaAddress) public{
@@ -282,7 +282,7 @@ contract Pledge{
         if(order.user==address(0)){
             return 0;
         }
-        if(order.expirationTime<block.timestamp){
+        if(block.timestamp<order.threeMonthsAfter){
             return 0;
         }
         if(block.timestamp>order.expirationTime){
@@ -325,26 +325,32 @@ contract Pledge{
         uint256 orderId = _tokenIdTracker.current();
         uint256 timestamps  = block.timestamp;
         orders[orderId].threeMonthsAfter = timestamps.add(three_months);
-        orders[orderId].expirationTime = timestamps.add(three_months).add( 100 days);
-        orders[orderId].hundredth = pledgeBalance.div(2).div(100 days);
+        orders[orderId].expirationTime = timestamps.add(three_months).add( 100 minutes);
+        orders[orderId].hundredth = pledgeBalance.div(2).div(100 minutes);
         orders[orderId].user= msg.sender;
         orders[orderId].balance = pledgeBalance;
+        orders[orderId].createTime = timestamps;
         userOrders[msg.sender].orderList.push(orderId);
         users[msg.sender].totalBalnaces = users[msg.sender].totalBalnaces.add(pledgeBalance);
-        users[ msg.sender].totalReleaseBalance = users[msg.sender].totalBalnaces.sub(users[msg.sender].totalConfirmAmount);
-
+        users[msg.sender].totalReleaseBalance = users[msg.sender].totalBalnaces.sub(users[msg.sender].totalConfirmAmount);
     }
-    function receiveAward(uint256 orderId) public{
+    function receiveAward(uint256 orderId,uint256 balanceReceive) public{
         Order storage order  = orders[orderId];
         uint256 orderReceiveAward = releaseBalance(orderId);
         if(orderReceiveAward==0){
             return ;
         }
+
         require(order.confirmAmount<order.balance);
         uint256 balance = orderReceiveAward.sub(order.confirmAmount);
+        if(balanceReceive>balance){
+            return ;
+        }
         require(qua.transfer(order.user,balance));
         order.confirmAmount = orderReceiveAward;
         users[ order.user].totalConfirmAmount = users[ order.user].totalConfirmAmount.add(orderReceiveAward);
         users[ order.user].totalReleaseBalance = users[ order.user].totalReleaseBalance.sub(orderReceiveAward);
     }
+
+
 }
